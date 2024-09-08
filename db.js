89bd2386b2a1110837b9ef7cb1297b4e8914b0c6
gabrielgnardy <gabriel.nardy@gmail.com>
2024-09-08 15:29:13 -0300

    fix db connect to vercel

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
+import mysql2 from 'mysql2';
 
 dotenv.config();
-
 const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   host: process.env.MYSQL_HOST,
   dialect: 'mysql',
+  dialectModule: mysql2
 });
 
 sequelize.authenticate()
